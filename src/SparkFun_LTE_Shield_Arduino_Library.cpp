d21ee8fe523a740f94a82b74843ad691be45eb12
Nathan Seidle <nathan@sparkfun.com>
2019-09-24 16:38:46 -0600

    Correcting pointer bug
    Original code failed to compile on platforms with tighter compiler restrictions (ie Artemis, Teensy, etc). This fixes the error and fixes a potential bug as well.

--- src/SparkFun_LTE_Shield_Arduino_Library.cpp
+++ src/SparkFun_LTE_Shield_Arduino_Library.cpp
@@ -741,7 +741,7 @@ LTE_Shield_error_t LTE_Shield::getAPN(String *apn, IPAddress *ip)
             {
                 // Fill in the APN:
                 searchPtr = strchr(searchPtr, '\"'); // Move to first quote
-                while ((*(++searchPtr) != '\"') && (searchPtr != '\0'))
+                while ((*(++searchPtr) != '\"') && (*searchPtr != '\0'))
                 {
                     apn->concat(*(searchPtr));
                 }
@@ -952,7 +952,7 @@ LTE_Shield_error_t LTE_Shield::getOperator(String *oper)
                 }
                 else
                 {
-                    while ((*(++searchPtr) != '\"') && (searchPtr != '\0'))
+                    while ((*(++searchPtr) != '\"') && (*searchPtr != '\0'))
                     {
                         oper->concat(*(searchPtr));
                     }
